b895850bde96d4ca1c10a08eef262d5c1c0f1257
Grass <movie7650@naver.com>
2025-10-31 16:26:53 +0900

    feat : fileupload

--- src/main/java/com/xray21/refsys/web/controller/FileController.java
+++ src/main/java/com/xray21/refsys/web/controller/FileController.java
@@ -1,7 +1,12 @@
 package com.xray21.refsys.web.controller;
 
+import com.xray21.refsys.web.dto.request.MemberJoinRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileController {
@@ -9,4 +14,15 @@ public class FileController {
     public String file() {
         return "file";
     }
+
+    @PostMapping("/upload")
+    public String upload(MultipartFile file) {
+        System.out.println("FileController.upload " + file);
+        return "ok";
+
+    }
+
 }
+
+
+
