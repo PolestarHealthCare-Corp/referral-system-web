edc581a84c67c901c2b35443c47875a7fd94ff16
윤상영 <movie7650@naver.com>
2025-11-02 16:13:07 +0900

    feat : 파일 로컬 경로에 업로드기능 구현 완료

--- src/main/java/com/xray21/refsys/web/controller/FileController.java
+++ src/main/java/com/xray21/refsys/web/controller/FileController.java
@@ -7,6 +7,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+
 
 @Controller
 public class FileController {
@@ -17,8 +20,20 @@ public class FileController {
 
     @PostMapping("/upload")
     public String upload(MultipartFile file) {
+        String uploadDir = "C:/download/";//파일 저장 경로
+        String fileName = file.getOriginalFilename(); //파일 원본 이름 가져오기
+        File destination = new File(uploadDir + fileName); // 자바가 제공해주는 File 라이브러리를 통해
+        try{
+            if (!destination.getParentFile().exists()){
+                destination.getParentFile().mkdirs();
+            }
+            file.transferTo(destination);
+            System.out.println("파일업로드완료:"+ destination.getAbsolutePath());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         System.out.println("FileController.upload " + file);
-        return "ok";
+        return "home";
 
     }
 
